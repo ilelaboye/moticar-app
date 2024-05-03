950fad9514e99ae7fac688344a962cbc15498f1e
Ilelaboye Lekan <40622920+ilelaboye@users.noreply.github.com>
2024-05-03 10:07:05 +0100

    Update splash_screen.dart

--- lib/splash/splashscreen/splash_screen.dart
+++ lib/splash/splashscreen/splash_screen.dart
@@ -25,7 +25,7 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   void initState() {
     super.initState();
-    getOnboardingScreen();
+    // getOnboardingScreen();
   }
 
   Future<void> getOnboardingScreen() async {
